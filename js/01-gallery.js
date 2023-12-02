9e6e87818e0db2a5a017ea08fbb5e3252d79ed24
Esnobart <offrichterrr@gmail.com>
2023-12-02 21:44:56 +0000

    Summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const galleryDiv = document.querySelector(".gallery");
 
-function galleryItem(arr) {
-    return arr.map(({ preview, original, description }) => {
-        <li class="gallery__item">
-           <a class="gallery__link" href="large-image.jpg">
-              <img
-                   class="gallery__image"
-                   src="${preview}"
-                   data-source="${original}"
-                   alt="${description}"
-               />
-           </a>
-        </li>
-    })
-}
 console.log(galleryItems);
 
-const galleryMarkup = galleryItem(galleryItems).join('');
-galleryDiv.insertAdjacentHTML('beforeend', galleryMarkup);
+const galleryDiv = document.querySelector(".gallery");
+
+
+galleryDiv.insertAdjacentHTML("beforeend", galleryItem(galleryItems));
+galleryDiv.addEventListener("click", handleClick);
+
+function galleryItem(arr) {
+    return arr.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    ).join('');
+}
+
+function handleClick(event) {
+    event.preventDefault()
+
+    if (event.target === event.currentTarget) {
+        return;
+    }
+
+    const image = event.target.closest(".gallery__link").getAttribute("href");
+
+    const modal = basicLightbox.create(`
+       <img class="gallery__image" src="${image}" alt="${image.description}" />
+    `)
+
+    modal.show()
+
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            return modal.close();
+        }
+
+        return;
+    })
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector(".gallery");
+
+galleryDiv.insertAdjacentHTML("beforeend", galleryItem(galleryItems));
+galleryDiv.addEventListener("click", handleClick);
+
+function galleryItem(arr) {
+    return arr.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    ).join('');
+}
+
+function handleClick(event) {
+    event.preventDefault()
+
+    if (event.target === event.currentTarget) {
+        return;
+    }
+
+    const overlay = new SimpleLightBox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+    overlay.on(`show.simpleLightBox`, function () {
+        `
+       <img class="gallery__image " src="${galleryItems.original}" alt="${galleryItems.description}">
+       `
+    });
+
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            return modal.close();
+        }
+
+        return;
+    })
+}
